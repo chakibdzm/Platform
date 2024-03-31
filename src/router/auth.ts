bd32db64e245d9eda3eb7f9fd9e694f4413e8de0
mohamed <ma.belhadj@esi-sba.dz>
2024-03-31 15:13:34 +0100

    add swagger documentation

--- src/router/auth.ts
+++ src/router/auth.ts
@@ -5,7 +5,42 @@ import { signup } from '../controllers/userController';
 
 const authrouter = express.Router();
 
-authrouter.post('/signup', signup);
-authrouter.post('/signin', signin);
+authrouter.post('auth/signup', signup);
+authrouter.post('auth/signin', signin);
 
 export default authrouter;
+
+
+/**
+ * @swagger
+ * /auth/signin:
+ *   post:
+ *     summary: Sign in a user
+ *     tags:
+ *       - Authentication
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       '200':
+ *         description: User signed in successfully
+ *         headers:
+ *           Authorization:
+ *             schema:
+ *               type: string
+ *               format: Bearer {token}
+ *       '401':
+ *         description: Invalid credentials
+ *       '404':
+ *         description: User not found
+ *       '500':
+ *         description: Internal server error
+ */
--- src/router/challenges.ts
+++ src/router/challenges.ts
@@ -17,3 +17,140 @@ ChallengeRouter.get('/api/verse/:verse_id/challenges/:id',[middleware.Auth,middl
 //submit & check flag
 ChallengeRouter.post('/api/submit/:id',[middleware.Auth,middleware.limiter] ,SubmitChallenge)
 
+
+/**
+ * @swagger
+ * /api/verses:
+ *   get:
+ *     summary: Get all verses
+ *     tags:
+ *       - Challenges
+ *     responses:
+ *       '200':
+ *         description: A list of verses
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Verse'
+ *       '500':
+ *         description: Internal server error
+ */
+
+/**
+ * @swagger
+ * /api/verse/{id}:
+ *   get:
+ *     summary: Get challenges by verse ID
+ *     tags:
+ *       - Challenges
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID of the verse
+ *     responses:
+ *       '200':
+ *         description: A list of challenges for the specified verse
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Challenge'
+ *       '404':
+ *         description: Verse not found
+ *       '500':
+ *         description: Internal server error
+ */
+
+/**
+ * @swagger
+ * /api/verse/{verse_id}/completed:
+ *   get:
+ *     summary: Check if verse is completed
+ *     tags:
+ *       - Challenges
+ *     parameters:
+ *       - in: path
+ *         name: verse_id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID of the verse
+ *     responses:
+ *       '200':
+ *         description: Message indicating verse completion
+ *       '404':
+ *         description: Verse not found or not completed
+ *       '500':
+ *         description: Internal server error
+ */
+
+/**
+ * @swagger
+ * /api/verse/{verse_id}/challenges/{id}:
+ *   get:
+ *     summary: Get challenge by ID
+ *     tags:
+ *       - Challenges
+ *     parameters:
+ *       - in: path
+ *         name: verse_id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID of the verse
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID of the challenge
+ *     responses:
+ *       '200':
+ *         description: Challenge details
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Challenge'
+ *       '404':
+ *         description: Challenge not found
+ *       '500':
+ *         description: Internal server error
+ */
+
+/**
+ * @swagger
+ * /api/submit/{id}:
+ *   post:
+ *     summary: Submit a challenge
+ *     tags:
+ *       - Challenges
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID of the challenge
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               flag:
+ *                 type: string
+ *     responses:
+ *       '200':
+ *         description: Congratulations message if flag is correct
+ *       '404':
+ *         description: Challenge not found
+ *       '500':
+ *         description: Internal server error
+ */
