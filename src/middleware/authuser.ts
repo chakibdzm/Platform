86e0dde580e238bcb33183e619a09fe9de6c3379
mohamed <ma.belhadj@esi-sba.dz>
2024-04-25 17:40:16 +0100

    hope last version :(

--- src/middleware/authuser.ts
+++ src/middleware/authuser.ts
@@ -30,25 +30,19 @@ export const middleware={
     Auth :async (req: CustomRequest, res: Response, next: NextFunction) => {
     
       
-        if (req.user) {
-            // If user is already authenticated, proceed to the next middleware
-            next();
-        } else {
-            // If req.user is not set, authenticate the user
-            const token = req.headers.authorization?.split(' ')[1];
-            if (!token) {
-                return res.status(401).json({ error: 'Unauthorized: No token provided' });
-            }
-            try {
-                const decodedToken: any = jwt.verify(token, process.env.JWT_SECRET!);
-                req.user = decodedToken;
-                next(); 
-            } catch (error) {
-                console.error('Error decoding token:', error);
-                return res.status(401).json({ error: 'Unauthorized: Invalid token' });
-            }
+        const token = req.headers.authorization?.split(' ')[1];
+        if (!token) {
+            return res.status(401).json({ error: 'Unauthorized: No token provided' });
         }
-        
+        try {
+            
+            const decodedToken: any = jwt.verify(token, process.env.JWT_SECRET!);
+            req.user = decodedToken;
+            next(); 
+        } catch (error) {
+            console.error('Error decoding token:', error);
+            return res.status(401).json({ error: 'Unauthorized: Invalid token' });
+        }
     }
 
 }
